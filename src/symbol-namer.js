ab0ead9d73ab2e755e61e1c84e4944f477fd6207
forestlin1212 <linshs@qq.com>
2019-03-26 13:23:17 +0800

    rename the unexpect names
    for the layers without text override, use the SymbolMaster name

--- src/symbol-namer.js
+++ src/symbol-namer.js
@@ -12,6 +12,13 @@ export function onRename(context) {
   } else {
     let renamedCount = 0;
     selectedLayers.forEach(layer => {
+      //get SymbolMaster name
+      var layerSymbolMasterName = layer.master.name;
+      var lastSlashIndex = layerSymbolMasterName.lastIndexOf('/');
+      if(lastSlashIndex > -1){ //get short SymbolMaster name
+        layerSymbolMasterName = layerSymbolMasterName.substring(lastSlashIndex+1);
+      }
+      
       var overrides = layer.overrides.filter(override => override.property === 'stringValue' && override.sketchObject.isEditable());
       var overrideCount = overrides.length;
 
@@ -19,11 +26,16 @@ export function onRename(context) {
         layer.name = overrides[0].value;
         renamedCount++
       }
+      else if(layer.name != layerSymbolMasterName){
+        //for the layers without text override, use the SymbolMaster name
+        layer.name = layerSymbolMasterName;
+        renamedCount++
+      }
     });
     switch (renamedCount) {
       case 0:
-      UI.message("None of the selected symbols have text overrides");
-      break;
+        UI.message("None of the selected symbols renamed");
+        break;
       case 1:
         UI.message("1 symbol renamed");
         break;
